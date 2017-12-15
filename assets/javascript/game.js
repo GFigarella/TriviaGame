04b7f50aff2a56cdc0c515ae524125a56b1abc96
Gabriel Figarella <GFigarella@Gabriels-MacBook-Pro.local>
2017-12-15 09:09:27 -0500

    fixed the issues where the next question was not displaying. Everything fine. Timer not showing after reset. Buttons not clearing values after repetitive clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -50,10 +50,12 @@ $(document).ready(function() {
     $("#display").hide();
 
     $("#start").on('click',function(){
+        questionCount = 3;
         $("#start").hide();
         $("#display").show();
-        intervalId = setInterval(decrement,1000);
-        showQuestions();
+        $("#startAgain").hide();
+        $(".result").hide();
+        showQuestion1();
 
     });
 
@@ -67,6 +69,7 @@ $(document).ready(function() {
             alert("time's up!");
             incorrect++;
             correctAnswer(2);
+            questionCount--;
             stop();
         }
     }
@@ -76,11 +79,11 @@ $(document).ready(function() {
         //  We just pass the name of the interval
         //  to the clearInterval function to stop the timer
         clearInterval(intervalId);
-     }
+    }
 
-    function nextPage(){
+    function nextPage(x){
         setTimeout(function(){console.log("Test Time Out")}, 2000);
-        delayId = setTimeout(showQuestions, 3000);
+        delayId = setTimeout(showQuestion2, 3000);
     }
 
     // 
@@ -90,129 +93,211 @@ $(document).ready(function() {
         if (x == 1){
             console.log("Correct: " + correct);
             if (questionCount == 3){
-                $("#display").html("Correct! It was Naughty Dog");
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#question").html("Correct! It was Naughty Dog");
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(showQuestion2, 1000 * 3);
             }
 
             else if (questionCount == 2){
-                $("#display").html("Correct! It was on 1996");
+                $("#question").html("Correct! It was on 1996");
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(showQuestion3, 1000 * 3);
             }
 
             else if (questionCount == 1){
-                $("#display").html("Correct! It was on 1993");
+                $("#questions").html("Correct! It was on 1993");
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(resultPage, 1000 * 3);
             }
         }
 
         else if (x == 2) {
             console.log("Incorrect: " + incorrect);
             if (questionCount == 3){
-                $("#display").html("The correct answer was " + questions.question1.a1);
+                $("#question").html("The correct answer was " + questions.question1.a1);
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(showQuestion2, 1000 * 3);
             }
 
             else if (questionCount == 2){
-                $("#display").html("The correct answer was " + questions.question2.a3);
+                $("#question").html("The correct answer was " + questions.question2.a3);
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(showQuestion3, 1000 * 3);
             }
 
             else if (questionCount == 1){
-                $("#display").html("The correct answer was " + questions.question3.a3);
+                $("#question").html("The correct answer was " + questions.question3.a3);
+                $("#timeLeft").hide();
+                $(".buttons").hide();
+                $("#answer1").empty();
+                $("#answer2").empty();
+                $("#answer3").empty();
+                $("#answer4").empty();
+                setTimeout(resultPage, 1000 * 3);
             }
-
         }
         stop();
-        nextPage();
+    }
+
+    function resultPage(){
+        //reset function, empties/hides everything and shows the results, and then when the #startAgian button is clicked, it goes back to the starting point.
+
+        $(".result").show();
+        $("#startAgain").show();
+        $("#timeLeft").empty();
+        $(".buttons").hide();
+        $("#question").empty();
+        $("#answer1").empty();
+        $("#answer2").empty();
+        $("#answer3").empty();
+        $("#answer4").empty();
+
+        $("#correctCount").html("<b>Number of correct answers: <b>" + correct);
+        $("#incorrectCount").html("<b>Number of incorrect answers: <b>" + incorrect);
+
+        $("#startAgain").on('click', function(){
+            correct = 0;
+            incorrect = 0;
+            $("#display").hide();
+            $("#start").show();
+            $(".buttons").show();
+        })
     }
 
     
-    function showQuestions(){
+    function showQuestion1(){
+        $("#timeLeft").html("5");
+        time = 5;
+        intervalId = setInterval(decrement,1000);
 
-        if (questionCount == 3){
-            $("#question").html(questions.question1.q);
-            $("#answer1").html(questions.question1.a1);
-            $("#answer2").html(questions.question1.a2);
-            $("#answer3").html(questions.question1.a3);
-            $("#answer4").html(questions.question1.a4);
+        $("#question").html(questions.question1.q);
+        $("#answer1").html(questions.question1.a1);
+        $("#answer2").html(questions.question1.a2);
+        $("#answer3").html(questions.question1.a3);
+        $("#answer4").html(questions.question1.a4);
 
-            $( "input[type='radio']").on("click",function(){
-                // Registering the click on any answer button to a variable
-                var answerId = $(this).attr("id");
-                console.log("You clicked " + answerId);
-                
-            // After clicking the submit button, check if the chosen answer was correct or not
-            $("#checkAnswer").on("click", function(){
-                console.log("Submit");
-                //increase correctCount if the answer is correct
-                if (answerId == "a1"){
-                    stop();
-                    correct++;
-                    console.log(questionCount);
-                    correctAnswer(1);
-                    questionCount--; // goes to correctAnswer, which displays the answer to the question
-                }
-                // increase incorrectCount if the answer is incorrect
-                // look into unbind
-                else{
-                    stop();
-                    incorrect++;
-                    console.log(questionCount);
-                    correctAnswer(2); // goes to correctAnswer, which displays the answer to the question
-                    questionCount--;
-                }
-                
-                // correctAnswer();
-                $("#checkAnswer").off('click'); 
-            })
-            })
-        }
 
-        else if (questionCount == 2){
-            $("#question").html(questions.question2.q);
-            $("#answer1").html(questions.question2.a1);
-            $("#answer2").html(questions.question2.a2);
-            $("#answer3").html(questions.question2.a3);
-            $("#answer4").html(questions.question2.a4);
+        $( "input[type='radio']").on("click",function(){
+            // Registering the click on any answer button to a variable
+            var answerId = $(this).attr("id");
+            $( "input[type='radio']").off('click'); // unbind clicks on the answer buttons
+            
+        // After clicking the submit button, check if the chosen answer was correct or not
+        $("#checkAnswer").on("click", function(){
+            console.log("Submit");
+            //increase correctCount if the answer is correct
+            if (answerId == "a1"){
+                stop();
+                correct++;
+                correctAnswer(1);
+                questionCount--; // goes to correctAnswer, which displays the answer to the question 
+                console.log("Question Count is: " + questionCount);
+            }
+            // increase incorrectCount if the answer is incorrect
+            // look into unbind
+            else{
+                stop();
+                incorrect++;
+                console.log(questionCount);
+                correctAnswer(2); // goes to correctAnswer, which displays the answer to the question
+                questionCount--;
+            }
+            // correctAnswer();
+            $("#checkAnswer").off('click'); 
+        })
+        })
+    }
+    
+    function showQuestion2(){
+        $("#timeLeft").html("5");
+        time = 5;
+        intervalId = setInterval(decrement,1000);
+        
+        $(".buttons").show();
+        $("#timeLeft").show();
+        $("#question").html(questions.question2.q);
+        $("#answer1").html(questions.question2.a1);
+        $("#answer2").html(questions.question2.a2);
+        $("#answer3").html(questions.question2.a3);
+        $("#answer4").html(questions.question2.a4);
 
-            $( "input[type='radio']").on("click",function(){
-                // Registering the click on any answer button to a variable
-                var answerId = $(this).attr("id");
-                console.log("You clicked " + answerId);
-                
-            // After clicking the submit button, check if the chosen answer was correct or not
-            $("#checkAnswer").on("click", function(){
-                console.log("Submit");
-                //increase correctCount if the answer is correct
-                if (answerId == "a3"){
-                    stop();
-                    correct++;
-                    correctAnswer(1); // goes to correctAnswer, which displays the answer to the question
-                    questionCount--;
-                }
-                // increase incorrectCount if the answer is incorrect
-                // look into unbind
-                else{
-                    stop();
-                    incorrect++;
-                    correctAnswer(2); // goes to correctAnswer, which displays the answer to the question;
-                    questionCount--;
-                }
-                $("#checkAnswer").off('click');
-            })
-            })
-        }
+        
 
-        else if (questionCount == 1){
+        $( "input[type='radio']").on("click",function(){
+            // Registering the click on any answer button to a variable
+            var answerId = $(this).attr("id");
+            $( "input[type='radio']").off('click'); //unbind click on the answer buttons
+            
+        // After clicking the submit button, check if the chosen answer was correct or not
+        $("#checkAnswer").on("click", function(){
+            console.log("answerId is: " + answerId);
+            //increase correctCount if the answer is correct
+            if (answerId == "a3"){
+                stop();
+                correct++;
+                correctAnswer(1); // goes to correctAnswer, which displays the answer to the question
+                questionCount--;
+            }
+            // increase incorrectCount if the answer is incorrect
+            // look into unbind
+            else{
+                stop();
+                incorrect++;
+                correctAnswer(2); // goes to correctAnswer, which displays the answer to the question;
+                questionCount--;
+            }
+            $("#checkAnswer").off('click');
+        })
+        })
+    }
+
+    function showQuestion3(){
+            $("#timeLeft").html("5");
+            time = 5;
+            intervalId = setInterval(decrement,1000);
+            
+            $(".buttons").show();
+            $("#timeLeft").show();
+
             $("#question").html(questions.question3.q);
             $("#answer1").html(questions.question3.a1);
             $("#answer2").html(questions.question3.a2);
             $("#answer3").html(questions.question3.a3);
             $("#answer4").html(questions.question3.a4);
 
-            $( "input[type='radio']").on("click",function(){
+            $("input[type='radio']").on("click",function(){
                 // Registering the click on any answer button to a variable
                 var answerId = $(this).attr("id");
-                console.log("You clicked " + answerId);
+                $("input[type='radio']").off('click');
                 
             // After clicking the submit button, check if the chosen answer was correct or not
             $("#checkAnswer").on("click", function(){
-                console.log("Submit");
+                console.log("submit");
                 //increase correctCount if the answer is correct
                 if (answerId == "a3"){
                     stop();
@@ -231,7 +316,6 @@ $(document).ready(function() {
                 $("#checkAnswer").off('click');
             })
             })
-        }
     };
 
 
